85daad4c9af4798984a587e4f9621851862e700b
Fletcher <fletcbet1660@gmail.com>
2024-05-29 04:51:57 +1200

    rename casks

---
+++ brew.nix
@@ -0,0 +1,20 @@
+{ ... }: 
+{
+  homebrew = {
+    enable = true;
+
+    casks = [
+      "syncthing"
+      "eclipse-java"
+      "discord"
+      "onedrive"
+      "blender"
+      "firefox"
+      "linearmouse"
+      "heroic"
+      "steam"
+      "betterdispaly"
+      "stats"
+    ];
+  };
+}
