4dde253251d4c61fee390f29b9eb8bc6bc429925
Fletcher <fletcbet1660@gmail.com>
2024-07-12 08:15:56 +1200

    add finder .nix file

--- modules/darwin/finder.nix
+++ modules/darwin/finder.nix
@@ -1,4 +1,13 @@
 { ... }:
 {
-  
+  system.defaults.finder = {
+    AppleShowAllExtensions = true; # show file extensions
+    AppleShowAllFiles = true; # show hidden files
+    CreateDesktop = true; # show desktop icons
+    FXDefaultSearchScope = "SCcf"; # search uses current folder
+    FXPreferredViewStyle = "Nlsv"; # deafault view is list view
+    ShowPathbar = true;
+    ShowStatusBar = true;
+    _FXShowPosixPathInTitle = true;
+  };
 }
